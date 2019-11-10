612ac7afae4655f1186fcb4f17854e3821e102db
Amitesh-2001 <55495888+Amitesh-2001@users.noreply.github.com>
2019-11-10 17:23:20 +0530

    Add files via upload

---
+++ doublelinkedlist.c
@@ -0,0 +1,201 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *llink;
+    struct node *rlink;
+};
+struct node *start = NULL;
+
+void insertbeg()
+{
+    struct node *temp ;
+    temp = (struct node*)malloc(sizeof(struct node));
+    printf("enter the data\n");
+    scanf("%d", &temp->data);
+    temp->llink=NULL;
+    temp->rlink=NULL;
+
+    if(start == NULL)
+        start = temp;
+    else
+    {
+        temp->rlink=start;
+        start->llink=temp;
+        start=temp;
+    }
+}
+void insertend()
+{
+    struct node *temp ;
+    temp = (struct node*)malloc(sizeof(struct node));
+    printf("enter the data\n");
+    scanf("%d", &temp->data);
+    temp->llink=NULL;
+    temp->rlink=NULL;
+
+    if(start == NULL)
+        start = temp;
+    else
+    {
+        struct node *p;
+        p=start;
+        while(p->rlink!=NULL)
+            p=p->rlink;
+        temp->llink=p;
+        p->rlink=temp;
+    }
+}
+void insertpos()
+{
+    int pos,count=0;
+    struct node *p;
+    p=start;
+    while(p!=NULL)
+    {
+        count++;
+        p=p->rlink;
+    }
+
+    printf("enter the pos\n");
+    scanf("%d",&pos);
+
+    if(pos==1)
+        insertbeg();
+    else if(pos==count+1)
+        insertend();
+    else if(pos>1&&pos<=count)
+    {
+        struct node *temp ;
+        int i;
+        temp = (struct node*)malloc(sizeof(struct node));
+        printf("enter the data\n");
+        scanf("%d", &temp->data);
+        temp->llink=NULL;
+        temp->rlink=NULL;
+        p=start;
+        for(i =1;i<pos-1;i++)
+        {
+            p=p->rlink;
+        }
+        temp->rlink=p->rlink;
+        p->rlink=temp;
+        temp->llink=p;
+        temp->rlink->llink=temp;
+    }
+    else
+        printf("invalid location\n");
+
+}
+void deletebeg()
+{
+    struct node *p;
+    if(start == NULL)
+        printf("empty\n");
+    else if(start->rlink==NULL)
+        start=NULL;
+    else
+    {
+        p=start;
+        p->llink=NULL;
+        start=p->rlink;
+        free(p);
+    }
+}
+void deleteend()
+{
+    struct node *p;
+    if(start == NULL)
+        printf("empty\n");
+    else if(start->rlink==NULL)
+        start=NULL;
+    else
+    {
+        p=start;
+        while(p->rlink!=NULL)
+            p=p->rlink;
+        p->llink->rlink=NULL;
+        free(p);
+    }
+}
+void deletepos()
+{
+     int pos,count=0;
+    struct node *p;
+    p=start;
+    while(p!=NULL)
+    {
+        count++;
+        p=p->rlink;
+    }
+
+    printf("enter the pos\n");
+    scanf("%d",&pos);
+
+    if(pos==1)
+       deletebeg();
+    else if(pos==count)
+        deleteend();
+    else if(pos>1&&pos<count)
+    {
+        int i;
+        p=start;
+        for(i =1;i<pos;i++)
+        {
+            p=p->rlink;
+        }
+        p->llink->rlink=p->rlink;
+        p->rlink->llink=p->llink;
+        free(p);
+    }
+    else
+        printf("invalid location\n");
+}
+void display()
+{
+    if(start == NULL)
+        printf("empty\n");
+    else
+    {
+        struct node *p;
+        p=start;
+        while(p!=NULL)
+        {
+            printf("%d-->",p->data);
+            p=p->rlink;
+        }
+        printf("\n");
+    }
+}
+
+void main()
+{
+    int ch;
+    while(1)
+    {
+        printf("1.insertbeg\n2.display\n3.insertend\n4.insertpos\n5.deletebeg\n6.deleteend\n7.deletepos\n8.exit\n");
+        printf("enter the choice\n");
+        scanf("%d",&ch);
+
+        switch(ch)
+        {
+            case 1 : insertbeg();
+                     break;
+            case 2 : display();
+                     break;
+            case 3 : insertend();
+                     break;
+            case 4 : insertpos();
+                     break;
+            case 5 : deletebeg();
+                     break;
+            case 6 : deleteend();
+                     break;
+            case 7 : deletepos();
+                     break;
+            case 8 : exit(0);
+        }
+    }
+}
